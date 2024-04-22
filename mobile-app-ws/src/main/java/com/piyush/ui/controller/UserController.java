8eba54e9bb2e81933951c00b1add4d04ab816bb2
Piyush Chhawachharia <pichhawachharia891@gmail.com>
2024-04-22 19:56:27 +0530

    GET Endpoint-Added response type as object

--- mobile-app-ws/src/main/java/com/piyush/ui/controller/UserController.java
+++ mobile-app-ws/src/main/java/com/piyush/ui/controller/UserController.java
@@ -1,21 +1,48 @@
 package com.piyush.ui.controller;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.service.annotation.DeleteExchange;
+
+import com.piyush.ui.model.response.UserRest;
 
 @RestController
-@RequestMapping("users")
+@RequestMapping("/users")
 public class UserController {
 	
 	@GetMapping
-	public String getUser() {
+	public String getUser(@RequestParam(value="page",defaultValue = "1") int page,@RequestParam(value="limit",defaultValue="50") int limit) {
 		
-		return "get User was called";
+		return "get User was called with page= "+page;
+	}
+	
+	
+	@GetMapping(path="/{userId}")
+	public ResponseEntity<UserRest> getUser(@PathVariable("userId") String userId) {
+		
+		UserRest returnValue=new UserRest();
+		returnValue.setFirstName("Piyush");
+		returnValue.setLastName("Chh");
+		returnValue.setEmail("p@gmail.com");
+		returnValue.setUserId(userId);
+		
+		return new ResponseEntity<UserRest>(HttpStatus.BAD_REQUEST);
+		
+//		return new ResponseEntity<UserRest>(returnValue,HttpStatus.OK);
+		
+		
+		
+		
 	}
 	
 	@PostMapping
